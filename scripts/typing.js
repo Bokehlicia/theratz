f8b830e8772ada3b02d3dea89216b79d6d0c169e
Bokehlicia <bokehlicia@gmail.com>
2023-12-24 18:16:23 +0100

    files removed to the trash

--- scripts/typing.js
+++
@@ -1,7 +0,0 @@
-import TypeIt from "typeit";
-
-new TypeIt("#multipleStrings", {
-  strings: ["This is a great string.", "But here is a better one."],
-  speed: 12,
-  waitUntilVisible: true,
-}).go();
