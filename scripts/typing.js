3851f4b41f16fc513f9b2bb938933250fd73f227
Bokehlicia <bokehlicia@gmail.com>
2023-12-24 18:14:24 +0100

    typing animation on the main section

---
+++ scripts/typing.js
@@ -0,0 +1,7 @@
+import TypeIt from "typeit";
+
+new TypeIt("#multipleStrings", {
+  strings: ["This is a great string.", "But here is a better one."],
+  speed: 12,
+  waitUntilVisible: true,
+}).go();
